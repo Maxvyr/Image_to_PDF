55aea31f16d0a972b972f92535941e8b549a589c
Maxvyr <maxvy_@naver.com>
2021-07-22 16:06:13 +0200

    change name pdf file with the hour of creation

--- lib/utils/pdf_api.dart
+++ lib/utils/pdf_api.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
@@ -8,6 +9,10 @@ import 'package:pdf/widgets.dart' as pw;
 
 class PdfApi {
   static Future<File> generateImage(XFile imageFile) async {
+    // generate name with the hour when the fiel generate
+    final now = DateTime.now();
+    final name = DateFormat("yyyy-MM-dd_Hmms").format(now);
+    // generate document
     final pdf = pw.Document();
     final image = pw.MemoryImage(
       File(imageFile.path).readAsBytesSync(),
@@ -32,7 +37,7 @@ class PdfApi {
       ),
     );
 
-    return saveDocument(name: "pdf_example.pdf", pdf: pdf);
+    return saveDocument(name: "$name.pdf", pdf: pdf);
   }
 
   static Future<File> saveDocument({
