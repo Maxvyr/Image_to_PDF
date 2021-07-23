b306b6e3e7dd57dd22324a3e7532c1da6547e6d5
Maxvyr <maxvy_@naver.com>
2021-07-23 09:41:05 +0200

    remove unuse package

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image_to_pdf/views/pdf_generate_page.dart';
 
